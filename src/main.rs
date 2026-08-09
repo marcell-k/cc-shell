1620e354bcdaae3f2cfc44d5832774d3e7943679
Kele98-star <kele.marcell.mail@gmail.com>
2026-08-09 21:23:48 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@
 use std::io::{self, Write};
 
 fn main() {
+    print!("$ ");
+    io::stdout().flush().unwrap();
     let mut input = String::new();
 
     io::stdin().read_line(&mut input).unwrap();
-    print!("$ ");
     println!("{}: command not found", input.trim());
-    io::stdout().flush().unwrap();
 }
